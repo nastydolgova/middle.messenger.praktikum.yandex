6259c5673ef7d07d2135f41923e92ebcf6220aae
nastydolgova <nasty.dolgova@gmail.com>
2022-09-13 23:57:52 +0300

    fix remark

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const port = 3000
 app.use('/', express.static('dist'))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Chat listening on port ${port}`)
 })
