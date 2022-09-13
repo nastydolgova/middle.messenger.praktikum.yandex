4a177c47bb4303e3d2ffb18f134074fcc26da067
nastydolgova <nasty.dolgova@gmail.com>
2022-09-13 18:54:42 +0300

    change server.js

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-const express = require('express');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const app = express();
-const PORT = 3000;
-const path = require('path');
+app.use(express.static('dist'));
 
-app.use(express.static('./dist'));
-
-app.get( /(.*?)/, (req, res) => {
-    res.sendFile(__dirname + '/dist/index.html');
-
-});
-
-app.listen(PORT, function() {
-    console.log(`Your server available at http://localhost:${PORT}`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
